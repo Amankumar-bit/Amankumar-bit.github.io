96a482f2b8cec0cabbc1123d804014d962fbf288
AMAN KUMAR <51264474+Amankumar-bit@users.noreply.github.com>
2020-06-21 15:02:23 +0530

    commiting first time
    first time commiting for WeCbr

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function on_submit(){
+
+document.getElementById('registration-logo').style.animation = "move4i 4ms linear  infinite";
+document.getElementById('registration-logo').style.webkitAnimation = "move4i 4ms linear  infinite";
+
+}
